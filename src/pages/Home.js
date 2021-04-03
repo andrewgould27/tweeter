3a3b052041392c22b69b2adcd678772e8c1caa2b
Andrew Gould <agould45@ku.edu>
2021-04-03 18:33:39 -0500

    Redesigned home page to entice prospective tweeters.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,24 @@ export default class Home extends Component
     render()
     {
         return(
-            <div>
-                <h1>Welcome to Tweeter</h1>
-                <p>
-                    <Link to="/login">Login</Link> or <Link to="/signup">Sign up!</Link>
-                </p>
-            </div>
+            <section className="hero is-medium is-link">
+                <div className="hero-body">
+                    <div className="columns">
+                        <div className="column is-half is-offset-one-quarter">
+                            <p className="title">Welcome to Tweeter!</p>
+                            <p className="subtitle">Your favorite off-brand social networking site!</p>
+                            <p className="my-2">
+                                Join today and start chatting with fellow Tweeters about the stuff that matters most, like what celebrities are doing.
+                            </p>
+                            <p className="has-text-right">
+                                <a className="button is-rounded is-outlined mr-1" href="/signup">Sign-up</a>
+                                <a className="button is-rounded is-outlined" href="/login">Log-in</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         )
     }
 }
